34c03de060410998d66aa5206a3b8330c086e657
sagrvma <notsagarverma@gmail.com>
2025-10-09 08:46:41 +0530

    defined core types: tile and grid

---
+++ src/features/pathfinding/types.ts
@@ -0,0 +1,13 @@
+export interface Tile {
+  row: number;
+  col: number;
+  isStart: boolean;
+  isEnd: boolean;
+  isWall: boolean;
+  isVisited: boolean;
+  isPath: boolean;
+  distance: number;
+  parent: Tile | null;
+}
+
+export type Grid = Tile[][];
